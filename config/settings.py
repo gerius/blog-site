7803d24ea71bbbbeb590fb9018a47bccec47e30e
gerius <geriusfouyet@gmail.com>
2021-05-17 17:16:57 +0100

    update settings

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,10 @@ DATABASES = {
         'PORT': '5432',
     },
 
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 # Password validation
